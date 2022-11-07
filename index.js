a2b9f4815ed40b482639595a5b92f2b725182a5a
Jonatan <jonatanangel88@gmail.com>
2022-11-07 02:20:46 -0300

    Chats

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ const main = async () => {
         }
       });
     });
+
+    console.log(`Server started on PORT ${PORT} DB connection successfull `);
   } catch (error) {
     return console.log(`ERROR db app.js ${error.message}`);
   }
